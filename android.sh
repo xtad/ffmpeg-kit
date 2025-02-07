c4f42e944d8b69a6d90ff388e17e6614ef22bce0
Jindřich Jakubička <j.jakubicka@email.cz>
2025-02-07 17:20:50 +0100

    fix: correct command in android.sh to publish all publications

--- android.sh
+++ android.sh
@@ -396,7 +396,7 @@ if [[ -n ${ANDROID_ARCHITECTURES} ]]; then
 
     # BUILD ANDROID ARCHIVE
     rm -f "${BASEDIR}"/android/ffmpeg-kit-android-lib/build/outputs/aar/ffmpeg-kit-release.aar 1>>"${BASEDIR}"/build.log 2>&1
-    ./gradlew ffmpeg-kit-android-lib:clean ffmpeg-kit-android-lib:assembleRelease ffmpeg-kit-android-lib:testReleaseUnitTest ffmpeg-kit-android-lib:publishAllPublicationsToReposiliteRepositoryRepository -PReposiliteRepositoryUsername="${MAVEN_NAME}" -PDomainRepositoryPassword="${MAVEN_SECRET}" 1>>"${BASEDIR}"/build.log 2>&1
+    ./gradlew ffmpeg-kit-android-lib:clean ffmpeg-kit-android-lib:assembleRelease ffmpeg-kit-android-lib:testReleaseUnitTest publishAllPublicationsToReposiliteRepositoryRepository -PReposiliteRepositoryUsername="${MAVEN_NAME}" -PDomainRepositoryPassword="${MAVEN_SECRET}" 1>>"${BASEDIR}"/build.log 2>&1
     if [ $? -ne 0 ]; then
       exit 1
     fi
